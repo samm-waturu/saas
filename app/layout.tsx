59076e0d2c3d485560f2e1bb81103e86bf97c498
Samm <velop.acc@gmail.com>
2023-08-31 10:10:27 +0300

    mounted the clerk provider and added the middleware directory

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+// Main parent
+import "@/app/global.css";
+import { FontFetchHeebo, FontFetchSans } from "./fonts/fonts";
+import { ClerkProvider } from '@clerk/nextjs'
+export default function RootLayout({children} : {
+    children: React.ReactNode
+}){
+return (
+  <ClerkProvider>
+      <html suppressHydrationWarning lang="en">
+      <body className={FontFetchHeebo.className}>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `try{if (!localStorage.mode) {
+                localStorage.mode = 'dark';
+              }
+              if (!localStorage.custom_panel) {
+                localStorage.custom_panel = '';
+              }
+              document.documentElement.setAttribute("data-style-skin", localStorage.mode);
+              if(localStorage.custom_panel !== ''){
+                document.documentElement.classList.add(localStorage.custom_panel);
+              }} catch(_){}`
+          }}
+        />
+        {children}
+        </body>
+        </html>
+  </ClerkProvider>
+  
+)
+}
